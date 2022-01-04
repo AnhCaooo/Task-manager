3d0f495c1b3e3938e44fe814e662c56fd231313d
Anh Cao <anhcao563@gmail.com>
2022-01-04 11:56:15 +0200

    Added file upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = new express.Router()
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 
 // User REST
 // Fetch all users (authenticated)
@@ -89,5 +91,50 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+// Setup endpoint for avater upload
+const upload = multer({
+    dest: 'avatar', 
+    limits: {
+        fileSize: 1000000
+    }, 
+    fileFilter(req, file, callback){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return callback(new Error('Please upload an image!'))
+        }
+
+        callback(undefined, true)
+    }
+})
+
+// Add avatar
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+// Delete avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+// Fetch all avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (error){
+        res.status(404).send()
+    }
+})
 
 module.exports = router
